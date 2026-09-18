917c5c127f83bb522eb3cc566ff7c2c6c23466e7
dragopress <elbyoutydragopress@gmail.com>
2026-09-18 18:38:17 +0100

    fix(api): wire AuthModule into guarded feature modules

--- apps/api/src/analytics/analytics.module.ts
+++ apps/api/src/analytics/analytics.module.ts
@@ -2,4 +2,7 @@ import {Module} from '@nestjs/common';
 import {AnalyticsController} from './analytics.controller';
 import {AnalyticsService} from './analytics.service';
 import {PrismaService} from '../common/prisma.service';
-@Module({controllers:[AnalyticsController],providers:[AnalyticsService,PrismaService]}) export class AnalyticsModule {}
+import {AuthModule} from '../auth/auth.module';
+
+@Module({imports:[AuthModule],controllers:[AnalyticsController],providers:[AnalyticsService,PrismaService]})
+export class AnalyticsModule {}
